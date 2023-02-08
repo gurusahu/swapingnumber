535ea66a4bcf09accaa286a0e0b214757ca3a07d
Ramesh Karre <Ramesh Karre@DESKTOP-LV4OVN9>
2023-02-08 17:00:42 +0530

    modifed code to addition of two numbers

--- src/test/java/practice/Swapingnumber.java
+++ src/test/java/practice/Swapingnumber.java
@@ -3,14 +3,11 @@ package practice;
 public class Swapingnumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int a = 12;
 		int b = 15;
-		a = a + b;
-		b = a - b;
-		a = a - b;
-System.out.println("swaping two number"+ a+" "+b);
+		int c = a - b;
+		System.out.println("subtraction of two numbers is:" + c);
 	}
 
 }
