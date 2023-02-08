452ab1c7875dcaf5a5fef7b78e64e63da414f51e
gurup <gurup@192.168.0.118>
2023-02-08 15:58:55 +0530

    My SwapingNumber project.

---
+++ src/test/java/practice/Swapingnumber.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Swapingnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 12;
+		int b = 15;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+System.out.println("swaping two number"+ a+" "+b);
+	}
+
+}
